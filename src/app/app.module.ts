c70784ec3de290fd9cfd83d92560170dda24cd9a
Orestarxx <oresto322@gmail.com>
2022-10-25 02:19:16 +0300

    homeWork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './configs/components/posts/posts.component';
+import {HttpClientModule} from "@angular/common/http";
+import { PostComponent } from './configs/components/post/post.component';
+import { CommentsComponent } from './configs/components/comments/comments.component';
+import { CommentComponent } from './configs/components/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    CommentsComponent,
+    CommentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/configs/urls.ts
@@ -0,0 +1,8 @@
+const baseURL = 'https://jsonplaceholder.typicode.com';
+
+const urls = {
+  posts:`${baseURL}/posts`,
+  comments:`${baseURL}/comments`
+}
+
+export {urls};
